e93877e139a235b7837a3cb3dbb460c683f4dd0f
bismahAbdullah <i202364@nu.edu.pk>
2022-11-11 21:46:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import picS from './sample.gif'
+import { useState } from 'react';
+
+
+//import './App.css';
+//import Button from 'react-bootstrap/Button';
 
 function App() {
+
+  const [pic, showPic] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={{ backgroundColor: '#2266ff', width: '2048px', height: '1005px' }}>
+      <div className="pt-5 pb-5 text-center">
+        <Button variant="outline-dark" size='lg' onClick={() => showPic(!pic)}>Press me to cherish joy</Button>
+      </div>
+
+      <div>
+        {
+          pic && (
+            <div className='text-center'>
+              <img src={picS} alt="Logo" />
+
+            </div>
+
+          )
+        }
+      </div>
+
+
+
+
+
+
     </div>
   );
 }
